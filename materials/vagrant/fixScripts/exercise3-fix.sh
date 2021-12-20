ea5a96bc1550f0f4a14d7009b664836619bada30
amos <amoshochman1982@gmail.com>
2021-12-20 15:59:01 +0200

    removing space

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i 's/denied/granted/g'  /etc/apache2/sites-available/000-default.conf
+sed -i 's/denied/granted/g' /etc/apache2/sites-available/000-default.conf
 service  apache2 reload 
