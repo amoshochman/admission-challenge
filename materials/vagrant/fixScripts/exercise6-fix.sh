eae25dfef22229c464d17f7b03b4bcdb35d7920e
amos <amoshochman1982@gmail.com>
2022-01-02 10:56:59 -0300

    workign exercise 6 draft

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,30 @@
-#!/bin/bash
-#add fix to exercise6-fix here
+#we need to check if we can first ssh and then copy everything
+#we need to create temporary folder in wise way
+
+dest_folder="${@: -1}"
+temp_folder="/vagrant/tmp_amos/"
+hostname=$(hostname)
+
+mkdir $temp_folder
+
+if [ $hostname == "server1" ]
+then
+  dest_server="server2"
+else
+  dest_server="server1"
+fi
+
+while (( $# > 1 ))
+do
+  cp $1 $temp_folder
+  command="sudo cp "
+  command+=$temp_folder
+  command+="$(basename -- $1)"
+  command+=" "
+  command+=$dest_folder
+  ssh vagrant@$dest_server $command
+  shift
+done
+
+rm -r $temp_folder
+
