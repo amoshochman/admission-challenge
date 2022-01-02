ad237fada93f9c20c3d7f515ee63a406ef88715c
amos <amoshochman1982@gmail.com>
2022-01-02 11:49:56 -0300

    second commit exercise 6

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,11 +1,10 @@
 #we need to check if we can first ssh and then copy everything
-#we need to create temporary folder in wise way
+#we need to check if we can call the script from both places it's requested to
 
 dest_folder="${@: -1}"
-temp_folder="/vagrant/tmp_amos/"
+temp_folder=$(mktemp -d --tmpdir=/vagrant)/
 hostname=$(hostname)
-
-mkdir $temp_folder
+#bytes=0
 
 if [ $hostname == "server1" ]
 then
@@ -17,14 +16,15 @@ fi
 while (( $# > 1 ))
 do
   cp $1 $temp_folder
-  command="sudo cp "
-  command+=$temp_folder
-  command+="$(basename -- $1)"
-  command+=" "
+  command="sudo cp ${temp_folder}"
+  command+="$(basename -- $1) "
   command+=$dest_folder
   ssh vagrant@$dest_server $command
+  #bytes+=1
   shift
 done
+
+#echo $bytes
 
 rm -r $temp_folder
 
