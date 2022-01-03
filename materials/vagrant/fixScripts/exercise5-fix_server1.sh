a0f3fb501ec2e860f6677d45a4ee2a270cded24e
Amos <amos@shieldfc.com>
2022-01-03 16:42:45 -0300

    repairing previous error

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 sudo -u vagrant -i <<'EOF'
 
-yes | ssh-keygen -t rsa -f /vagrant/id_rsa -q -N ''
+ssh-keygen -t rsa -f /vagrant/id_rsa -q -N ''
 
 cp /vagrant/id_rsa /home/vagrant/.ssh/id_rsa
 chmod 0600 /home/vagrant/.ssh/id_rsa
 
-cat /home/vagrant/.ssh/id_rsa
-
 cat /vagrant/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/authorized_keys
 
-touch ~/.ssh/config
-echo 'Host server*' > ~/.ssh/config
-echo '  StrictHostKeyChecking no' >> ~/.ssh/config
-echo '  UserKnownHostsFile=/dev/null' >> ~/.ssh/config
-
 EOF
+
+#touch ~/.ssh/config
+#echo 'Host server*' > ~/.ssh/config
+#echo '  StrictHostKeyChecking no' >> ~/.ssh/config
+#echo '  UserKnownHostsFile=/dev/null' >> ~/.ssh/config
--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -4,14 +4,12 @@ sudo -u vagrant -i <<'EOF'
 cp /vagrant/id_rsa /home/vagrant/.ssh/id_rsa
 chmod 0600 /home/vagrant/.ssh/id_rsa
 
-cat /home/vagrant/.ssh/id_rsa
-
 cat /vagrant/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/authorized_keys
 
-touch ~/.ssh/config
-echo 'Host server*' > ~/.ssh/config
-echo '  StrictHostKeyChecking no' >> ~/.ssh/config
-echo '  UserKnownHostsFile=/dev/null' >> ~/.ssh/config
-
 EOF
+
+#touch ~/.ssh/config
+#echo 'Host server*' > ~/.ssh/config
+#echo '  StrictHostKeyChecking no' >> ~/.ssh/config
+#echo '  UserKnownHostsFile=/dev/null' >> ~/.ssh/config
